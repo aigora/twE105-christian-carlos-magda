c9f6dae18b345997a7aaacd7cd18fb2068a335ac
christianclementedefrutos <47789982+christianclementedefrutos@users.noreply.github.com>
2019-04-10 20:17:28 +0200

    Update Menú_ inicio_v1.c

--- Menú_ inicio_v1.c
+++ Menú_ inicio_v1.c
@@ -6,7 +6,8 @@
 void main()
 {
 	system ("color F0");//cambia el color del fondo(F) y del texto(0).
-	char option;
+	//option1 es el caracter para seleccionar las opciones del menú de inicio y option2 es el caracter para seleccionar las opciones del catálogo
+	char option1, option2;
 	do
 	{
 		system("cls");//limpia la pantalla
@@ -17,23 +18,35 @@ void main()
  		printf("Seleccione una opci%cn para comenzar: \n\n \tAcceder al cat%clogo de productos (c)\n \tInicio de sesi%cn (i)\n \tRegistrarse (r)\n \tSalir (s)\n",162,160,162);
     
 		//Recoge la opción del usuario introducida por teclado
-    		scanf("%c",&option);
+    		scanf("%c",&option1);
     
     		//Opciones elegibles por el usuario
-    		switch(option)
+    		switch(option1)
     		{
     			case 'c':
     				{
     					system("cls");
     					printf("\n\tCAT%cLOGO\n",181);
-    					system("pause");
-    					break;
+    					printf("Opciones del cat%clogo:\n -Mostrar cat%clogo completo(c)\n -Filtrar por precio(p)\n -Solo ofertas(o)\n -Mostrar categor%cas(k)\n", 160,160,161);
+    					/* ERROR
+						scanf("%c",&option2);
+						switch(option2)
+						{
+							case 'c':
+							{
+								printf("\n\tCAT%cLOGO\n",181);
+								system("pause");
+								break;	
+							}	
+						}
+						*/ 				
+      					break;
 				}
     			case 'i':
     				{
     					system("cls");
     					printf("\n\tINICIO DE SESI%cN\n",224);
-    					system("pause");
+    					system("pause");//El programa hace una pausa, espera a que el usuario pulse una tecla para continuar.
     					break;
 	        		}
 			case 'r':
@@ -56,5 +69,5 @@ void main()
 				} 	
     		}
 	}	
-	while(option!='s');
-}}
+	while(option1!='s');
+}
