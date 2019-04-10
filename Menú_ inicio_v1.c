b995406603595b154150e9bd0571c876e5abc472
christianclementedefrutos <47789982+christianclementedefrutos@users.noreply.github.com>
2019-04-10 10:01:42 +0200

    Corrijo indentado.

--- Menú_ inicio_v1.c
+++ Menú_ inicio_v1.c
@@ -9,52 +9,52 @@ void main()
 	char option;
 	do
 	{
-	system("cls");//limpia la pantalla
-	//Pantalla de inicio
-    printf("\n\te-shop\n");
-    printf("----------------------------------------------------------\n");
-    printf("\n BIENVENIDO\n\n");
-    printf("Seleccione una opci%cn para comenzar: \n\n \tAcceder al cat%clogo de productos (c)\n \tInicio de sesi%cn (i)\n \tRegistrarse (r)\n \tSalir (s)\n",162,160,162);
+		system("cls");//limpia la pantalla
+		//Pantalla de inicio
+		printf("\n\te-shop\n");
+    		printf("----------------------------------------------------------\n");
+    		printf("\n BIENVENIDO\n\n");
+ 		printf("Seleccione una opci%cn para comenzar: \n\n \tAcceder al cat%clogo de productos (c)\n \tInicio de sesi%cn (i)\n \tRegistrarse (r)\n \tSalir (s)\n",162,160,162);
     
-	//Recoge la opción del usuario introducida por teclado
-    scanf("%c",&option);
+		//Recoge la opción del usuario introducida por teclado
+    		scanf("%c",&option);
     
-    //Opciones elegibles por el usuario
-    switch(option)
-    {
-    	case 'c':
+    		//Opciones elegibles por el usuario
+    		switch(option)
     		{
-    			system("cls");
-    			printf("\n\tCAT%cLOGO\n",181);
-    			system("pause");
-    			break;
-		    }
-    	case 'i':
-    		{
-    			system("cls");
-    			printf("\n\tINICIO DE SESI%cN\n",224);
-    			system("pause");
-    			break;
-			}
-		case 'r':
-			{
-				system("cls");
-				printf("\n\tCREA UNA CUENTA\n");
-				system("pause");
-				break;
-			}
-		case 's':
-			{
-				system("cls");
-				break;
-			}
-		default:
-			{
-				printf("\n\tEl caracter introducido no es v%clido\n",160);
-				system("pause");
-				break;
-			} 	
-    }
-	}
+    			case 'c':
+    				{
+    					system("cls");
+    					printf("\n\tCAT%cLOGO\n",181);
+    					system("pause");
+    					break;
+				}
+    			case 'i':
+    				{
+    					system("cls");
+    					printf("\n\tINICIO DE SESI%cN\n",224);
+    					system("pause");
+    					break;
+	        		}
+			case 'r':
+				{
+					system("cls");
+					printf("\n\tCREA UNA CUENTA\n");
+					system("pause");
+					break;
+				}
+			case 's':
+				{
+					system("cls");
+					break;
+				}
+			default:
+				{
+					printf("\n\tEl caracter introducido no es v%clido\n",160);
+					system("pause");
+					break;
+				} 	
+    		}
+	}	
 	while(option!='s');
 }
